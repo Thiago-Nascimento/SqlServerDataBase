1784c92fa3b7b84ef27d8059876edfeb794fc281
Thiago-Nascimento <thiago.r.nascimento@hotmail.com>
2019-10-09 14:00:02 -0300

    Update Aula db

--- DMLs.sql
+++ DMLs.sql
@@ -3,7 +3,11 @@ INSERT INTO aluno(Nome, Ra, Idade) VALUES ('Thiago', 'R123', 21);
 INSERT INTO materia(Materia) VALUES ('Física');
 
 -- Alterar dados
+-- Fim de linha ;
 UPDATE aluno SET
 	Nome='Outro Nome',
 	Idade='25'
 WHERE IdAluno = 2;
+
+-- Excluir dado
+DELETE FROM materia WHERE IdMateria = 2;
--- DQLs.sql
+++ DQLs.sql
@@ -1,4 +1,15 @@
 -- Mostrar dados
 SELECT * FROM aluno;
-
 SELECT * FROM materia;
+
+-- Mostra dados de acordo com o parametro fornecido pelo WHERE
+SELECT * FROM aluno WHERE Idade = 21;
+
+-- Mostra dados em uma busca que contenha a palavra '%Thiago%', pode ser precedido e sucedido por qualquer coisa (%)
+SELECT * FROM aluno WHERE Idade LIKE '%Thiago%';
+
+-- Ordena por nome do aluno de forma ascendente
+SELECT * FROM aluno ORDER BY Nome ASC;
+
+-- Ordena por nome do aluno de forma decrescente
+SELECT * FROM aluno ORDER BY Nome DESC;
